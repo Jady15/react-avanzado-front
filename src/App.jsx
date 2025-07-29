57dc2ca26af7bc5ad28005ce11542137f5bc584a
Isaac Luviano <isaacluvianohernandez@gmail.com>
2025-07-29 15:38:08 -0700

    Conexión de la app con el backend

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import PostList from './pages/PostList';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import CreatePost from './pages/CreatePost';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<PostList/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <Route path='/create' element={<CreatePost/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
